c88f569be1bcbd6298e1780f25289b97c2c8b604
gustsucc <gustsucc@gmail.com>
2021-06-27 22:25:46 -0400

    Pantalla Estudiante

--- src/main/java/org/infor/FXMLEstudiante.java
+++ src/main/java/org/infor/FXMLEstudiante.java
@@ -2,7 +2,6 @@ package org.infor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import com.gluonhq.charm.glisten.control.TextField;  
 import javafx.fxml.Initializable;
 
 public class FXMLEstudiante implements Initializable{
